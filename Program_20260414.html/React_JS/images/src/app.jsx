e30d68c15e1c50a915491abafa4ee7b9fdf6c94d
Johnny <chuaboonhock@singnet.com.sg>
2026-05-30 18:33:03 +0800

    insert new react learn files - images & links-anchors

--- Program_20260414.html/React_JS/images/src/app.jsx
+++ Program_20260414.html/React_JS/images/src/app.jsx
@@ -1,9 +1,15 @@
 import "./app.css";
+import AssestImage from "./components/Assestimage";
+import InlineImage from "./components/InlineImage";
+import PublicImage from "./components/PublicImage";
 
 export function App() {
   return (
     <>
       <h1> images </h1>
+      <InlineImage />
+      <AssestImage />
+      <PublicImage />
     </>
   );
 }
---
+++ Program_20260414.html/React_JS/links-anchors/src/components/AboutUs.jsx
@@ -0,0 +1,13 @@
+import { Link } from "react-router-dom";
+
+function AboutUs() {
+  return (
+    <>
+      <h1>About Us</h1>
+      <hr />
+      <Link to="/">Home</Link>
+    </>
+  );
+}
+
+export default AboutUs;
